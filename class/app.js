1338c8060ccfed3b5c186fb3f1f02e0371d5b0be
stalin <svalarezo62@gmail.com>
2023-01-12 12:28:52 -0500

    formulario simple cors

--- class/app.js
+++ class/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors"
 import * as dotenv from 'dotenv' 
 dotenv.config()
 import { db } from "../db/databaseMongo.js";
@@ -16,6 +17,7 @@ export class Server {
   }
 
   middleware(){
+    this.app.use(cors())
     this.app.use(express.json())
   };
 
